9c35bc10c45ffa24a5b32f2459621670c2504706
Sulaiman Shamasna | BIVAL GmbH <sulaiman.shamasna@bival.de>
2024-02-29 17:32:15 +0100

    feat: :sparkles: Add a new feature to chat with information in a given url

---
+++ smart_chatbot.py
@@ -0,0 +1,82 @@
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain_community.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+class VectorDB:
+    """Class to manage document loading and vector database creation."""
+    
+    def __init__(self,
+                 directory='docs/my_docs',
+                 glob='./*.pdf',
+                 data_format:str = None,
+                 url='https://en.wikipedia.org/wiki/General_relativity'
+                 ):
+        self.directory = directory
+        self.glob = glob
+        self.data_format = data_format
+        self.url = url
+
+    def load_documents(self, data_format):
+        if data_format.lower() == 'pdf':
+            loader = DirectoryLoader(self.directory, glob=self.glob, loader_cls=PyPDFLoader)
+            return loader.load()
+        elif data_format.lower() == 'url':
+            loader = WebBaseLoader(self.url)
+            return loader.load()
+        else:
+            return
+
+    def create_vector_db(self):
+        documents = self.load_documents(self.data_format)
+        if self.data_format.lower() == 'pdf':
+            text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+            texts = text_splitter.split_documents(documents)
+            embeddings = OpenAIEmbeddings()
+            return Chroma.from_documents(texts, embeddings)
+        elif self.data_format.lower() == 'url':
+            text_splitter = RecursiveCharacterTextSplitter()
+            document_chunks = text_splitter.split_documents(documents)   
+            return Chroma.from_documents(document_chunks, OpenAIEmbeddings()) 
+        else:
+            return 
+    
+class ConversationalRetrievalChain:
+    """Class to manage the QA chain setup."""
+
+    def __init__(self, model_name="gpt-3.5-turbo", temperature=0):
+        self.model_name = model_name
+        self.temperature = temperature
+
+    def create_chain(self):
+        model = ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
+
+        memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True
+            )
+        vector_db = VectorDB(data_format='url')
+        retriever = vector_db.create_vector_db().as_retriever(search_type="similarity", search_kwargs={"k": 2})
+
+        return RetrievalQA.from_chain_type(
+            llm=model,
+            retriever=retriever,
+            memory=memory,
+        )
+
+def main():
+    """Main function to execute the QA system."""
+    query = input('\n Q: ')
+    qa_chain = ConversationalRetrievalChain().create_chain()
+    result = qa_chain({"query": query})
+    print(result['result'])
+
+
+if __name__ == "__main__":
+    main()
--- streamlit_app_v1.py
+++ streamlit_app_v1.py
@@ -1,57 +1,64 @@
 from chatbot import QAChain
 import streamlit as st
 import os
-from chatbot_w_memory import ConversationalRetrievalChain
+# from chatbot_w_memory import ConversationalRetrievalChain
+from smart_chatbot import ConversationalRetrievalChain
 
-# App title
-st.set_page_config(page_title="🦜🔗💬 LangChain 1 Chatbot")
+import subprocess
 
-# Replicate Credentials
-with st.sidebar:
-    st.title('🦜🔗💬 LangChain 1 Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['OPENAI_API_KEY']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+def main():
+    # App title
+    st.set_page_config(page_title="🦜🔗💬 LangChain 1 Chatbot")
+
+    # Replicate Credentials
+    with st.sidebar:
+        st.title('🦜🔗💬 LangChain 1 Chatbot')
+        if 'OPENAI_API_KEY' in st.secrets:
+            st.success('API key already provided!', icon='✅')
+            replicate_api = st.secrets['OPENAI_API_KEY']
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
-os.environ['OPENAI_API_KEY'] = replicate_api
+            replicate_api = st.text_input('Enter Replicate API token:', type='password')
+            if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+                st.warning('Please enter your credentials!', icon='⚠️')
+            else:
+                st.success('Proceed to entering your prompt message!', icon='👉')
+        st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    os.environ['OPENAI_API_KEY'] = replicate_api
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    # Store LLM generated responses
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    # Display or clear chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    def clear_chat_history():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 
-def generate_openai_response(prompt_input):
-    qa_chain = ConversationalRetrievalChain().create_chain()
-    result = qa_chain({"query": prompt_input})
-    return result['result']
+    def generate_openai_response(prompt_input):
+        qa_chain = ConversationalRetrievalChain().create_chain()
+        result = qa_chain({"query": prompt_input})
+        return result['result']
 
-# User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
+    # User-provided prompt
+    if prompt := st.chat_input(disabled=not replicate_api):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_openai_response(prompt)
-            placeholder = st.empty()
-            placeholder.markdown(response)
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = generate_openai_response(prompt)
+                placeholder = st.empty()
+                placeholder.markdown(response)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)
+
+if __name__ == "__main__":
+    main()
